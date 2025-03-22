03a2fdd6232ac972c47bca9865b1329d9e55a0f4
Vladislav Dedushkin <v.dedushkin@miops.dev>
2025-03-22 14:15:43 +0300

    add cleanup and verbosity to migrations

--- migrations/V001__create_tables.sql
+++ migrations/V001__create_tables.sql
@@ -1,3 +1,11 @@
+-- Очистка
+
+DROP TABLE IF EXISTS public.order_product;
+DROP TABLE IF EXISTS public.orders;
+DROP TABLE IF EXISTS public.orders_date;
+DROP TABLE IF EXISTS public.product;
+DROP TABLE IF EXISTS public.product_info;
+
 -- Создание структуры
 
 CREATE TABLE public.order_product (
